367d1e77f2765f9169e73560086380c65df218ba
rfox12-edu <73962631+rfox12-edu@users.noreply.github.com>
2024-03-24 08:10:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import math
 st.title("Mortgage Repayments Calculator")
 
 st.write("### Input Data")
-df = pd.read_csv("Superstore_Sales.csv",  encoding='cp1252')
+df = pd.read_csv("Superstore_Sales.csv",  encoding="utf8", errors='ignore')
 st.dataframe(df)
 
 st.write("### Input Data 2")
