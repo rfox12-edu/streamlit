ad12b67e9584de88e2408dcf5abb275badf560ba
Robert Fox <73962631+rfox12-edu@users.noreply.github.com>
2025-03-17 14:47:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-st.title("Data App Assignment, on Oct 7th")
+st.title("Data App Assignment, on March 17th")
 
 st.write("### Input Data and Examples")
 df = pd.read_csv("Superstore_Sales_utf8.csv", parse_dates=True)
